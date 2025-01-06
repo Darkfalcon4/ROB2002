50e13a72388421aa441024591a87ab31fa63543f
Darkfalcon4 <benjaminstuart200@gmail.com>
2025-01-06 17:49:54 +0000

    create base world

--- src/rob2002_project/rob2002_project/move.py
+++ src/rob2002_project/rob2002_project/move.py
@@ -1,13 +1,14 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-
+import numpy as np
 class Moves(Node):
     def __init__(self):
         super().__init__('move')
 
         # Create a publisher for the cmd_vel topic
-        self.movement_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.publisher_= self.create_publisher(Twist, 'cmd_vel', 10)
+        # self.odom_publisher = self.create_publisher(,'cmd_vel',10)
 
         timer_period = 2.0  # seconds
         self.timer = self.create_timer(timer_period, self.moving)
@@ -15,20 +16,27 @@ class Moves(Node):
 
     def moving(self):
         twist_msg = Twist()
-
-        twist_msg.linear.x = 0.2
-        twist_msg.linear.z = 0.2
-
-        self.movement_publisher.publish(twist_msg)
+        while True:
+            msg = Twist()
+            msg.angular.z = -0.1
+            self.publisher_.publish(msg)
+    # def odomError(self, N):
+    #     MBE , STD , MAE , RMSE = 0 
+    #     ei = []  
+    #     for x in range (N) : 
+    #      ei.append(x)
+    #     self.odom_publisher.publish(ei)
+        
+        
 
 def main(args=None):
-    print('Starting move_square.py.')
 
     rclpy.init(args=args)
 
     movement = Moves()
 
     rclpy.spin(movement)
+   
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
